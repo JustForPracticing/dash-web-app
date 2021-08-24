40fc784791964be161ba43fc5ce12eabb2676352
JustForPracticing <87746967+JustForPracticing@users.noreply.github.com>
2021-08-24 18:15:59 +0530

    Update my_script.sh

--- my_script.sh
+++ my_script.sh
@@ -1,3 +1,4 @@
+set -x
 cd dash-web-app/resources/home/dnanexus/
 pip3 install --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host pypi.python.org dash
 python local_test.py
